3f9bda27ab1c25ffde6998f6c40c74654fd86ce5
NitisriGirkala <nitisrigirkala@gmail.com>
2025-04-02 19:33:42 -0500

    Unit testing Service Layer

---
+++ Spring-boot-tutorial/src/test/java/com/dailycodebuffer/Spring_boot_tutorial/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.dailycodebuffer.Spring_boot_tutorial.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
