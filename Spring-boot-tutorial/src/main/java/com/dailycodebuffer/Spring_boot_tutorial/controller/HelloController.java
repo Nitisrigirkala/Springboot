884c776ae9a0a2088e540d931daf04c8ac4089c6
NitisriGirkala <nitisrigirkala@gmail.com>
2025-04-05 20:01:08 -0500

    Adding Config in properties file

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Spring_boot_tutorial/controller/HelloController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Spring_boot_tutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.Spring_boot_tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 
    @GetMapping("/")
     public String helloWorld(){
-        return "Welcome to Daily Code Buffer!!";
+
+       return welcomeMessage;
     }
 
 }
